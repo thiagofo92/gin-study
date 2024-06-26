911051d03e6de9a0cb2b3123b52867a9088153d0
TFO <thiagofo92@gmail.com>
2024-06-26 11:57:02 -0300

    :memo: add swagger

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "thiagofo92/study-api-gin/web"
+import "thiagofo92/study-api-gin/internal/web"
 
+// @title           Swagger Example API
+// @version         0.2.0
+// @description     This is a sample server celler server.
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @host      localhost:3500
+// @BasePath  /api/v1
 func main() {
 	web.RunServer()
 }
